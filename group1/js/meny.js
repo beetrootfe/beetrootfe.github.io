d337c34cc914f86a12ba362b925897fc6fd08e11
ivan <ivangoodman36@gmail.com>
2020-04-01 12:05:44 +0300

    cookies

--- group1/js/meny.js
+++ group1/js/meny.js
@@ -1,7 +1,10 @@
 function openNav() {
-    document.getElementById("myNav").style.height = "100%";
-  }
-  
-  function closeNav() {
-    document.getElementById("myNav").style.height = "0%";
-  }
+  document.getElementById("myNav").style.height = "100%";
+}
+
+function closeNav() {
+  document.getElementById("myNav").style.height = "0%";
+}
+function closeCook() {
+  document.getElementById("cookies").style.display = "none";
+}
